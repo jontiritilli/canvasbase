a1d2c2b07e5fda705c8b71e58afd8c3ee12056c3
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-07-26 16:08:07 -0700

    updated dependencies, added AirBnB eslint rules, adjusted base font and css

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import Logo from '../assets/images/logo.svg';
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+  <div>
+    <div className="app">
+      <img src={Logo} alt="react logo" className="logo rotate" />
+      <h1>
+        {'Welcome to React'}
+      </h1>
     </div>
+  </div>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app';
+import App from './components/App';
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+  <App />,
+  document.getElementById('root'),
 );
