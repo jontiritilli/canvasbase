7214f3367f39d20b9b206f97287fdc9af5f8bc92
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-08-08 12:21:57 -0700

    removed auth file

--- server/auth/auth.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  unsplashKey: '64bcf6d1fa2c5c623e96486fdc5e47b1e00f25170cba854ec03a91b3ad5729a0',
-  unsplashSecret: '5d8e2ef1c989c19b1d8bd2f93b8c3c2ca2fcbe85e4d15916ad2370c9d279a9a7'
-}
