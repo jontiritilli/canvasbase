0cac26b59da263cfcb55809eca854879aa32ba3e
Jonathan Tiritilli <jonathantiritilli@gmail.com>
2018-12-19 14:45:02 -0800

    add window detection

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,18 +9,40 @@ class SearchBar extends Component {
 
   handleSearchByTerm(values){
     console.log(`search submitted ${values}`);
-    this.props.searchByTerm(values);
+    // this.props.searchByTerm(values);
+  }
+
+  getWindowHeight(){
+    return window.screen.height * window.devicePixelRatio
+  }
+
+  getWindowWidth(){
+    return window.screen.width * window.devicePixelRatio
   }
 
   render(){
     const { handleSubmit } = this.props;
+    const windowHeight = this.getWindowHeight();
+    const windowWidth = this.getWindowWidth();
+
     return (
       <div className='searchContainer row justify-content-md-center align-items-center'>
         <form className='col-sm-6' onSubmit={handleSubmit(this.handleSearchByTerm.bind(this))}>
           <div className='input-group mb-3'>
-            <Field className='form-control' name='canvasSearch' component={renderInput} type="text" placeholder='Find a Canvas' />
+            <Field className='form-control' name='canvasSearch' component={renderInput} type="text" placeholder='Enter Search Term' />
             <div className='input-group-append'>
               <button className="btn btn-outline-success">Let's Go</button>
+            </div>
+            <div className='input-group-append'>
+              <button className="btn btn-outline-warning">Random</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-group col-sm-6 mb-3">
+              <Field className='form-control' name='windowWidth' component={renderInput} type="text" placeholder={windowWidth} />
+            </div>
+            <div className="input-group col-sm-6 mb-3">
+              <Field className='form-control' name='windowHeight' component={renderInput} type="text" placeholder={windowHeight} />
             </div>
           </div>
         </form>
@@ -33,4 +55,4 @@ SearchBar = reduxForm({
   form: 'search',
 })(SearchBar);
 
-export default connect(null, {search})(SearchBar);
+export default connect(null, {})(SearchBar);
